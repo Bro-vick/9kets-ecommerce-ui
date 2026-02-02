1a783b5346125bcc38e4ecc411bcc41d2cecfbf8
George Victor <georgev421@gmail.com>
2026-02-02 06:57:41 +0100

    Added Responsiveness

--- src/components/filters/SidebarFilters.tsx
+++ src/components/filters/SidebarFilters.tsx
@@ -6,7 +6,20 @@ import AvailabilityFilter from "./AvailabilityFilter";
 
 export default function SidebarFilters() {
   return (
-    <aside className="bg-white rounded-xl p-6 space-y-8 sticky top-24 h-fit">
+    <aside
+      className="
+        hidden lg:block
+        bg-white
+        rounded-xl
+        p-6
+        space-y-8
+        sticky
+        top-32
+        self-start
+        max-h-[calc(100vh-8rem)]
+        overflow-y-auto
+      "
+    >
       <CategoryFilter />
       <PriceFilter />
       <BrandFilter />
